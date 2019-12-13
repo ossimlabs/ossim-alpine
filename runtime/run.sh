bffbb2ac7448c1c123bbb2b0e0d669de6c8ecbad
sbortman <scott.bortman@gmail.com>
2019-12-13 15:31:05 -0500

    fixed local postgresql issue

--- runtime/run.sh
+++ runtime/run.sh
@@ -6,12 +6,7 @@ DIST_DIR=$PWD/../dist
 mkdir -p $WORK_DIR
 mkdir -p $DIST_DIR
 
-HOST_IP=$(ifconfig | grep inet | grep netmask |grep broadcast | awk '{print $2}')
-
-echo $HOST_IP
-
 docker run -it --rm \
-  --add-host="localhost:${HOST_IP}" \
   -v $PWD:/scripts \
   -v $WORK_DIR:/work \
   -v $DIST_DIR:/usr/local \
