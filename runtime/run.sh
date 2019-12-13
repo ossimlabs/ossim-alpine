16b4c2b29f4c572562ed462c68be00ba2e67b16e
sbortman <scott.bortman@gmail.com>
2019-12-13 13:35:28 -0500

    testing running omar services

--- runtime/run.sh
+++ runtime/run.sh
@@ -6,4 +6,16 @@ DIST_DIR=$PWD/../dist
 mkdir -p $WORK_DIR
 mkdir -p $DIST_DIR
 
-docker run -it --rm -v $PWD:/scripts -v $WORK_DIR:/work -v $DIST_DIR:/usr/local -v $OSSIM_DATA:/data ossim-runtime-alpine-minimal:local 
+HOST_IP=$(ifconfig | grep inet | grep netmask |grep broadcast | awk '{print $2}')
+
+echo $HOST_IP
+
+docker run -it --rm \
+  --add-host="localhost:${HOST_IP}" \
+  -v $PWD:/scripts \
+  -v $WORK_DIR:/work \
+  -v $DIST_DIR:/usr/local \
+  -v $OSSIM_DATA:/data  \
+  -v $O2_DEV_HOME/omar-services/apps/omar-services-app:/omar-services \
+  -p 8081:8081 \
+  ossim-runtime-alpine-minimal:local 
