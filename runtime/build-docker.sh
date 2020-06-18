a65c7859d680b31760edec8faa568256111cbdbd
Tyler Culp <tylercculp@gmail.com>
2020-06-18 14:46:18 -0400

    Renamed the images created

--- runtime/build-docker.sh
+++ runtime/build-docker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ln ../compile-ossim/output/ossim-dist-minimal-alpine.tgz ./
-docker build -t ossim-runtime-minimal-alpine:local "$@" . 
+docker build -t ossim-runtime-alpine:local "$@" . 
 
